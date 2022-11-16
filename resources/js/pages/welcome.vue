239612a3914dda4e306a69aeb897ff8f796c2160
feynoobs <p.w.feeniej@gmail.com>
2022-11-16 17:00:30 +0900

    鳥忍法

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from "vue";
+
+defineProps({
+    greeting: String,
+});
+onMounted(() => {
+    console.log("Welcome Page mounted");
+    greeting = 'hage';
+});
+</script>
+<template>
+    <h1>{{ greeting }} Inertia.js</h1>
+</template>
